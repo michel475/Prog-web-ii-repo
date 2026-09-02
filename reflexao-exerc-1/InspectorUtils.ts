0e49fec1a136a81ace999b9ab0e56307c2129fe7
Ully <ullyvieira@hotmail.com>
2026-09-01 21:49:20 -0300

    feat: separating responsabilities on inspectorUtils

--- reflexao-exerc-1/InspectorUtils.ts
+++ reflexao-exerc-1/InspectorUtils.ts
@@ -1,14 +1,40 @@
 export class InspectorUtils { 
     constructor() {}
 
-    inspecionarObjeto(obj: any): void {
-        const atributos = Object.keys(obj);
+    inspecionarObjeto(obj: any): { attr: string[], prototipoObj: any, metodos: string[], className: string }   {
+        const attr = Object.keys(obj);
         const prototipoObj = Object.getPrototypeOf(obj);
         const metodos = Object.getOwnPropertyNames(prototipoObj).filter(prop => typeof prototipoObj[prop] === 'function' && prop !== 'constructor');
         const className = obj.constructor.name;
+        return { attr, prototipoObj, metodos, className }
+    }
+
+    searchMetodos(obj: any): string[] {
+        const { metodos } = this.inspecionarObjeto(obj);
+        const methods = [...metodos];
+        return methods;
+    }
+
+    searchAtributos(obj: any): string[] {
+        const { attr } = this.inspecionarObjeto(obj);
+        const atributos = [...attr];
+        return atributos;
+    }
+
+    getClassNameOf(obj: any): string {
+        const { className } = this.inspecionarObjeto(obj);
+        return className;
+    }
+
+    imprimirItens(arrayItens: string[]): void {
+        const joinedItens = arrayItens.join(", ");
+        console.log(joinedItens);
+    }
+
+    imprimirInspecao(itens: { attr: string[], metodos: string[], className: string }): void {
         console.log("=== Introspecção do Objeto ===")
-        console.log("Classe: ", className);
-        console.log("Atributos da Instância: ",atributos)
-        console.log("Métodos: ", metodos);
+        console.log("Classe:", itens.className);
+        console.log("Atributos da Instância:", itens.attr.join(", "));
+        console.log("Métodos:", itens.metodos.join(", "))
     }
 }
